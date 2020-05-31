9e1a21402dc4835d510a4e36b1d280a589d39360
nokchax <nokchax@gmail.com>
2020-05-31 17:40:19 +0900

    refactor: remove init method test

---
+++ src/test/java/bowling/state/StrikeTest.java
@@ -0,0 +1,33 @@
+package bowling.state;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.junit.jupiter.api.Assertions.*;
+
+@DisplayName("스트라이크 테스트")
+class StrikeTest {
+    private Strike STRIKE;
+
+    @BeforeEach
+    void setting() {
+        STRIKE = new Strike();
+    }
+
+    @Test
+    @DisplayName("초기화")
+    void init() {
+        assertThatCode(Strike::new).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("종료 상태는 true")
+    void isEnd() {
+        assertThat(STRIKE.isEnd()).isTrue();
+    }
+
+
+}
