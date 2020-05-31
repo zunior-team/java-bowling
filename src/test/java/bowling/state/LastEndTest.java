49e2b0b9da1e16bc186a7cfebf9e0b30c3dcacd4
nokchax <nokchax@gmail.com>
2020-06-01 07:15:09 +0900

    test: last end's isEnd() return true

--- src/test/java/bowling/state/LastEndTest.java
+++ src/test/java/bowling/state/LastEndTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.Test;
 
 import java.util.Stack;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 @DisplayName("마지막 프레임에서의 종료 상태 테스트")
@@ -14,5 +15,11 @@ class LastEndTest {
     @DisplayName("초기화")
     void init() {
         assertThatCode(() -> LastEnd.init(new Stack<>())).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("종료 상태는 true")
+    void isEnd() {
+        assertThat(LastEnd.init(new Stack<>()).isEnd()).isTrue();
     }
 }
