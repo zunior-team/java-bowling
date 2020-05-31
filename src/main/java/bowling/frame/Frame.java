40535e6d67d321b390d71fc630686aa325ae5f9e
nokchax <nokchax@gmail.com>
2020-06-01 07:53:36 +0900

    feat: frame down pins

--- src/main/java/bowling/frame/Frame.java
+++ src/main/java/bowling/frame/Frame.java
@@ -1,8 +1,15 @@
 package bowling.frame;
 
 import bowling.pin.Pin;
+import bowling.state.State;
 
 public abstract class Frame {
+    protected State state;
+
     public void downPins(final Pin downPins) {
+        this.state = state.downPins(downPins);
     }
+
+    public abstract boolean isFrameEnd();
+    public abstract boolean isBowlingEnd();
 }
--- src/test/java/bowling/frame/FrameTest.java
+++ src/test/java/bowling/frame/FrameTest.java
@@ -8,7 +8,17 @@ import static org.assertj.core.api.Assertions.assertThatCode;
 
 @DisplayName("프레임 테스트")
 class FrameTest {
-    private final Frame frame = new Frame() {};
+    private final Frame frame = new Frame() {
+        @Override
+        public boolean isFrameEnd() {
+            return false;
+        }
+
+        @Override
+        public boolean isBowlingEnd() {
+            return false;
+        }
+    };
 
     @Test
     @DisplayName("핀 쓰러뜨리기")
