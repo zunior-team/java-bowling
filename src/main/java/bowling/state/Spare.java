8a15c8d000b501ef60fc29ca0ad3c68e1def3548
nokchax <nokchax@gmail.com>
2020-05-31 18:40:36 +0900

    test: spare instance init with two pin instance

--- src/main/java/bowling/state/Spare.java
+++ src/main/java/bowling/state/Spare.java
@@ -1,4 +1,9 @@
 package bowling.state;
 
+import bowling.pin.Pin;
+
 public class Spare extends EndState {
+    public static Spare init(final Pin firstDownPins, Pin secondDownPins) {
+        return null;
+    }
 }
---
+++ src/test/java/bowling/state/SpareTest.java
@@ -0,0 +1,18 @@
+package bowling.state;
+
+import bowling.pin.Pin;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+@DisplayName("스페어 상태 테스트")
+class SpareTest {
+
+    @Test
+    @DisplayName("스페어는 두개의 핀 객체를 가지고 초기화한다")
+    void init() {
+        assertThatCode(() -> Spare.init(Pin.of(5), Pin.of(5))).doesNotThrowAnyException();
+    }
+
+}
