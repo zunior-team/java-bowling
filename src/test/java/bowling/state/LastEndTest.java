4de77b8c9f5d5154535c7354030b78fc7a4916e2
nokchax <nokchax@gmail.com>
2020-06-01 07:18:46 +0900

    test: init fail when stack is null or empty

--- src/test/java/bowling/state/LastEndTest.java
+++ src/test/java/bowling/state/LastEndTest.java
@@ -2,11 +2,15 @@ package bowling.state;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.jupiter.params.provider.NullAndEmptySource;
 
 import java.util.Stack;
+import java.util.stream.Stream;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.assertj.core.api.Assertions.assertThatCode;
+import static org.assertj.core.api.Assertions.*;
 
 @DisplayName("마지막 프레임에서의 종료 상태 테스트")
 class LastEndTest {
@@ -17,9 +21,25 @@ class LastEndTest {
         assertThatCode(() -> LastEnd.init(new Stack<>())).doesNotThrowAnyException();
     }
 
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("초기화 실패")
+    void initFail(final Stack<State> states) {
+        assertThatIllegalArgumentException()
+                .isThrownBy(() -> LastEnd.init(states));
+    }
+
+    private static Stream<Stack<State>> initFail() {
+        return Stream.of(
+                null,
+                new Stack<>()
+        );
+    }
+
     @Test
     @DisplayName("종료 상태는 true")
     void isEnd() {
         assertThat(LastEnd.init(new Stack<>()).isEnd()).isTrue();
     }
+
 }
