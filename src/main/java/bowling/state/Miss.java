bf37c35c41108b1eccc571368c8e91a1fd0fbdf2
nokchax <nokchax@gmail.com>
2020-05-31 18:56:09 +0900

    feat: miss init with two pin info

--- src/main/java/bowling/state/Miss.java
+++ src/main/java/bowling/state/Miss.java
@@ -3,8 +3,26 @@ package bowling.state;
 import bowling.pin.Pin;
 
 public class Miss extends EndState {
+    private final Pin firstDownPins;
+    private final Pin secondDownPins;
 
-    public static void init(Pin firstDownPins, Pin secondDownPins) {
+    private Miss(final Pin firstDownPins, final Pin secondDownPins) {
+        verify(firstDownPins, secondDownPins);
 
+        this.firstDownPins = firstDownPins;
+        this.secondDownPins = secondDownPins;
+    }
+
+    private void verify(final Pin firstDownPins, final Pin secondDownPins) {
+        boolean isAllDown = firstDownPins.add(secondDownPins)
+                .isAllDown();
+
+        if (isAllDown) {
+            throw new IllegalArgumentException("Miss state should contain not down pins");
+        }
+    }
+
+    public static Miss init(final Pin firstDownPins, final Pin secondDownPins) {
+        return new Miss(firstDownPins, secondDownPins);
     }
 }
