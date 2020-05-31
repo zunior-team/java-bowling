6212dbff2265756d981dab21e2e2aa6b3945205b
nokchax <nokchax@gmail.com>
2020-05-31 17:29:45 +0900

    test: instance return ready state

--- src/main/java/bowling/state/Ready.java
+++ src/main/java/bowling/state/Ready.java
@@ -3,6 +3,11 @@ package bowling.state;
 import bowling.pin.Pin;
 
 public class Ready extends State {
+
+    public static Ready instance() {
+        return null;
+    }
+
     @Override
     public State downPins(final Pin downPins) {
         if (downPins.isAllDown()) {
--- src/test/java/bowling/state/ReadyTest.java
+++ src/test/java/bowling/state/ReadyTest.java
@@ -41,4 +41,9 @@ class ReadyTest {
         assertThat(READY.downPins(Pin.of(9))).isInstanceOf(Running.class);
     }
 
+    @Test
+    @DisplayName("싱글톤 적용 instance")
+    void instance() {
+        assertThatCode(() -> Ready.instance()).doesNotThrowAnyException();
+    }
 }
