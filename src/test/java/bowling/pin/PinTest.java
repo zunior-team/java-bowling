85b82beae8c027f2e6435e070eff1ac1fcc4deb0
nokchax <nokchax@gmail.com>
2020-05-31 15:42:20 +0900

    test: equals test of pin

--- src/test/java/bowling/pin/PinTest.java
+++ src/test/java/bowling/pin/PinTest.java
@@ -1,9 +1,11 @@
 package bowling.pin;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 @DisplayName("볼링핀에 대한 테스트")
@@ -15,6 +17,12 @@ class PinTest {
     void init(final int fallenPins) {
         assertThatCode(() -> Pin.of(fallenPins))
                 .doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("equals: 싱글톤 패턴 적용")
+    void equals() {
+        assertThat(Pin.of(0)).isEqualTo(Pin.of(0));
     }
 
 }
