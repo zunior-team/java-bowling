4b22371798c7a7b92eb313f09a00f83d80dd4938
nokchax <nokchax@gmail.com>
2020-05-31 18:15:26 +0900

    test: end states's isEnd() return true

--- src/test/java/bowling/state/EndStateTest.java
+++ src/test/java/bowling/state/EndStateTest.java
@@ -5,6 +5,7 @@ import bowling.pin.Pin;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 @DisplayName("종료 스테이트에 대한 테스트")
@@ -16,5 +17,11 @@ class EndStateTest {
     void downPins() {
         assertThatExceptionOfType(UnReachableStateException.class)
                 .isThrownBy(() -> END_STATE.downPins(Pin.of(10)));
+    }
+
+    @Test
+    @DisplayName("종료 스테이트는 isEnd() 호출시 true를 반환한다.")
+    void isEnd() {
+        assertThat(END_STATE.isEnd()).isTrue();
     }
 }
