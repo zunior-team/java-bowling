ff6ca4ca0c2361c49ab4ea9f7099450205fd60be
nokchax <nokchax@gmail.com>
2020-06-01 08:12:04 +0900

    feat: init frame

--- src/main/java/bowling/frame/Frame.java
+++ src/main/java/bowling/frame/Frame.java
@@ -3,11 +3,21 @@ package bowling.frame;
 import bowling.pin.Pin;
 import bowling.state.State;
 
+import java.util.Objects;
+
 public abstract class Frame {
     protected State state;
 
     protected Frame(final State state) {
+        validate(state);
+
         this.state = state;
+    }
+
+    private void validate(State state) {
+        if (Objects.isNull(state)) {
+            throw new IllegalArgumentException("State can't be a null to init frame");
+        }
     }
 
     public void downPins(final Pin downPins) {
