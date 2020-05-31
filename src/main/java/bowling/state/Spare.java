2ecadc58e1683421d65e98d457aa989be7a7ea39
nokchax <nokchax@gmail.com>
2020-05-31 18:49:49 +0900

    fix: spare can init with one pin info

--- src/main/java/bowling/state/Spare.java
+++ src/main/java/bowling/state/Spare.java
@@ -3,27 +3,13 @@ package bowling.state;
 import bowling.pin.Pin;
 
 public class Spare extends EndState {
-    private final Pin firstDownPins;
-    private final Pin secondDownPins;
+    private final Pin downPins;
 
-    private Spare(final Pin firstDownPins, Pin secondDownPins) {
-        verify(firstDownPins, secondDownPins);
-
-        this.firstDownPins = firstDownPins;
-        this.secondDownPins = secondDownPins;
+    private Spare(final Pin downPins) {
+        this.downPins = downPins;
     }
 
-    private void verify(final Pin firstDownPins, final Pin secondDownPins) {
-        boolean isAllDown = firstDownPins.add(secondDownPins)
-                .isAllDown();
-
-        if (!isAllDown) {
-            throw new IllegalArgumentException("Spare must down all pins");
-        }
+    public static Spare init(final Pin downPins) {
+        return new Spare(downPins);
     }
-
-    public static Spare init(final Pin firstDownPins, Pin secondDownPins) {
-        return new Spare(firstDownPins, secondDownPins);
-    }
-
 }
--- src/test/java/bowling/state/SpareTest.java
+++ src/test/java/bowling/state/SpareTest.java
@@ -5,21 +5,13 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.assertj.core.api.Assertions.assertThatIllegalArgumentException;
 
 @DisplayName("스페어 상태 테스트")
 class SpareTest {
 
     @Test
-    @DisplayName("스페어는 두개의 핀 객체를 가지고 초기화한다")
+    @DisplayName("스페어는 하나의 핀 객체를 가지고 초기화한다")
     void init() {
-        assertThatCode(() -> Spare.init(Pin.of(5), Pin.of(5))).doesNotThrowAnyException();
-    }
-
-    @Test
-    @DisplayName("핀의 합이 핀의 토탈 갯수가 아닌 경우에는 예외가 발생한다")
-    void initFailSumIsNotSameWithTotalPins() {
-        assertThatIllegalArgumentException()
-                .isThrownBy(() -> Spare.init(Pin.of(5), Pin.of(2)));
+        assertThatCode(() -> Spare.init(Pin.of(5))).doesNotThrowAnyException();
     }
 }
