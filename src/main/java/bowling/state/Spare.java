ccdcf1c3892d66e956bfdc9776a20418f2bed292
nokchax <nokchax@gmail.com>
2020-05-31 18:43:42 +0900

    feat: spare instance init with two pin instance

--- src/main/java/bowling/state/Spare.java
+++ src/main/java/bowling/state/Spare.java
@@ -3,6 +3,20 @@ package bowling.state;
 import bowling.pin.Pin;
 
 public class Spare extends EndState {
+
+    private Spare(final Pin firstDownPins, Pin secondDownPins) {
+        verify(firstDownPins, secondDownPins);
+    }
+
+    private void verify(final Pin firstDownPins, final Pin secondDownPins) {
+        boolean isAllDown = firstDownPins.add(secondDownPins)
+                .isAllDown();
+
+        if (!isAllDown) {
+            throw new IllegalArgumentException("Spare must down all pins");
+        }
+    }
+
     public static Spare init(final Pin firstDownPins, Pin secondDownPins) {
         return null;
     }
