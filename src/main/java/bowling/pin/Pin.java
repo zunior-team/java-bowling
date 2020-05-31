f49e2869de85fd7e31d4762c7af7f06279ccc9a5
nokchax <nokchax@gmail.com>
2020-05-31 15:40:01 +0900

    feat: init pin test and apply singleton pattern

--- src/main/java/bowling/pin/Pin.java
+++ src/main/java/bowling/pin/Pin.java
@@ -1,6 +1,42 @@
 package bowling.pin;
 
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 // 볼링 핀
 public class Pin {
-    private int pin;
+    private static final int MINIMUM_SIZE_OF_PIN = 0;
+    private static final int MAXIMUM_SIZE_OF_PIN = 10;
+
+    private int fallenPins;
+
+    private Pin(final int fallenPins) {
+        verify(fallenPins);
+
+        this.fallenPins = fallenPins;
+    }
+
+    private static void verify(final int fallenPins) {
+        if (fallenPins < MINIMUM_SIZE_OF_PIN || MAXIMUM_SIZE_OF_PIN < fallenPins) {
+            throw new IllegalArgumentException(
+                    "Fallen pin size is invalid value must be range in " +
+                            MINIMUM_SIZE_OF_PIN + " ~ " + MAXIMUM_SIZE_OF_PIN
+            );
+        }
+    }
+
+    public static Pin of(final int fallenPins) {
+        verify(fallenPins);
+
+        return LazyHolder.PINS
+                .get(fallenPins);
+    }
+
+    private static class LazyHolder {
+        public static final List<Pin> PINS = IntStream.rangeClosed(MINIMUM_SIZE_OF_PIN, MAXIMUM_SIZE_OF_PIN)
+                .boxed()
+                .map(Pin::new)
+                .collect(Collectors.toList());
+    }
 }
---
+++ src/test/java/bowling/pin/PinTest.java
@@ -0,0 +1,20 @@
+package bowling.pin;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.assertj.core.api.Assertions.assertThatCode;
+
+@DisplayName("볼링핀에 대한 테스트")
+class PinTest {
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 10})
+    @DisplayName("볼링핀 초기화")
+    void init(final int fallenPins) {
+        assertThatCode(() -> Pin.of(fallenPins))
+                .doesNotThrowAnyException();
+    }
+
+}
