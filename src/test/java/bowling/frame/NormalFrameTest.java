14fa43102548c268954c0d745e5d44dfc1bee1f8
nokchax <nokchax@gmail.com>
2020-06-01 13:24:37 +0900

    test: append frame

--- src/test/java/bowling/frame/NormalFrameTest.java
+++ src/test/java/bowling/frame/NormalFrameTest.java
@@ -1,8 +1,13 @@
 package bowling.frame;
 
+import bowling.pin.Pin;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 @DisplayName("일반 프레임 테스트")
@@ -12,5 +17,20 @@ class NormalFrameTest {
     @DisplayName("초기화")
     void init() {
         assertThatCode(() -> NormalFrame.init()).doesNotThrowAnyException();
+    }
+
+    @Test
+    @DisplayName("다음 프레임 생성")
+    void addFrame() {
+        List<Frame> frames = new ArrayList<>();
+        Frame frame = NormalFrame.init();
+
+        frame.downPins(Pin.of(5));
+        frame.appendFrame(frames);
+        assertThat(frames).hasSize(0);
+
+        frame.downPins(Pin.of(5));
+        frame.appendFrame(frames);
+        assertThat(frames).hasSize(1);
     }
 }
