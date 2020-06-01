209b14afffe22747b165ef7fb788041560c32968
nokchax <nokchax@gmail.com>
2020-06-01 15:34:54 +0900

    feat: init frames

--- src/main/java/bowling/frame/Frames.java
+++ src/main/java/bowling/frame/Frames.java
@@ -1,8 +1,18 @@
 package bowling.frame;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Frames {
+    private final List<Frame> frames;
 
-    public static void init() {
+    private Frames() {
+        this.frames = new ArrayList<>();
 
+        frames.add(NormalFrame.init());
+    }
+
+    public static Frames init() {
+        return new Frames();
     }
 }
--- src/test/java/bowling/frame/FramesTest.java
+++ src/test/java/bowling/frame/FramesTest.java
@@ -4,7 +4,6 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThatCode;
-import static org.junit.jupiter.api.Assertions.*;
 
 @DisplayName("프레임 집합에 대한 테스트")
 class FramesTest {
@@ -12,6 +11,6 @@ class FramesTest {
     @Test
     @DisplayName("초기화")
     void init() {
-        assertThatCode(() -> Frames.init()).doesNotThrowAnyException();
+        assertThatCode(Frames::init).doesNotThrowAnyException();
     }
 }
