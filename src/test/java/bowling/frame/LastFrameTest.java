cfd148f6860e4ec92dfa78af53aa138f0decc8d8
nokchax <nokchax@gmail.com>
2020-06-01 15:22:44 +0900

    test: last frame's isBowlingEnd return depends on state

--- src/test/java/bowling/frame/LastFrameTest.java
+++ src/test/java/bowling/frame/LastFrameTest.java
@@ -1,8 +1,15 @@
 package bowling.frame;
 
+import bowling.pin.Pin;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.assertThatCode;
 
 @DisplayName("마지막 프레임 테스트")
@@ -12,6 +19,50 @@ class LastFrameTest {
     @DisplayName("초기화")
     void init() {
         assertThatCode(LastFrame::init).doesNotThrowAnyException();
+    }
+
+    @ParameterizedTest
+    @MethodSource
+    @DisplayName("볼링이 끝났는지")
+    void isBowlingEnd(final LastFrame lastFrame, final boolean expected) {
+        assertThat(lastFrame.isBowlingEnd()).isEqualTo(expected);
+    }
+
+    private static Stream<Arguments> isBowlingEnd() {
+        LastFrame miss = LastFrame.init();
+        miss.downPins(Pin.of(0));
+        miss.downPins(Pin.of(0));
+
+        LastFrame ready = LastFrame.init();
+
+        LastFrame running = LastFrame.init();
+        running.downPins(Pin.of(5));
+
+        LastFrame strike = LastFrame.init();
+        strike.downPins(Pin.of(10));
+
+        LastFrame doubleStrike = LastFrame.init();
+        doubleStrike.downPins(Pin.of(10));
+        doubleStrike.downPins(Pin.of(10));
+
+        LastFrame tripleStrike = LastFrame.init();
+        tripleStrike.downPins(Pin.of(10));
+        tripleStrike.downPins(Pin.of(10));
+        tripleStrike.downPins(Pin.of(10));
+
+        LastFrame spare = LastFrame.init();
+        spare.downPins(Pin.of(5));
+        spare.downPins(Pin.of(5));
+
+        return Stream.of(
+                Arguments.of(miss, true),
+                Arguments.of(ready, false),
+                Arguments.of(running, false),
+                Arguments.of(strike, false),
+                Arguments.of(doubleStrike, false),
+                Arguments.of(tripleStrike, true),
+                Arguments.of(spare, false)
+        );
     }
 
 }
