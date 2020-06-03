d0e1e81520fac18290aea9764282d37d66279c67
nokchax <nokchax@gmail.com>
2020-06-03 18:00:45 +0900

    feat: score and score first class collection

--- src/main/java/bowling/domain/score/Score.java
+++ src/main/java/bowling/domain/score/Score.java
@@ -1,7 +1,33 @@
 package bowling.domain.score;
 
-public class Score {
-    public static void init() {
+import java.util.ArrayList;
+import java.util.List;
 
+public class Score {
+    private static final Score NOT_CALCULATED_YET = new Score(-1);
+
+    private final int size;
+    private final List<Integer> score;
+
+    private Score(final int size) {
+        this.size = size;
+        score = new ArrayList<>();
     }
+
+    public static Score init() {
+        return new Score(2);
+    }
+
+    public static Score ofSpare() {
+        return new Score(3);
+    }
+
+    public static Score ofStrike() {
+        return new Score(3);
+    }
+
+    private boolean isCalculatable() {
+        return score.size() == size;
+    }
+
 }
---
+++ src/main/java/bowling/domain/score/Scores.java
@@ -0,0 +1,7 @@
+package bowling.domain.score;
+
+import java.util.List;
+
+public class Scores {
+    List<Score> scores;
+}
